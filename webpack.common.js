a65d75d7f2b777b6ac9b00a4c2168aa3d8e035a8
Anna Udziela <annaudziela@gmail.com>
2020-07-21 13:07:23 +0200

    new file ania.html

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/ania.html",
+      inject: true,
+      chunks: ["index", "another"],
+      filename: "ania.html",
+    }),
   ],
 };
